59dbadf1b9a72e6e695c93c2d219518f1b77c5a1
Niall Kennedy <niall@niallkennedy.com>
2012-05-31 00:02:25 +0000

    add up to 3 people ranked by interestingness for claimed companies with one or more confirmed people. refs #2

---
+++ api.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Talk to the AngelList API
+ *
+ * @since 1.1
+ */
+class AngelList_API {
+	/**
+	 * Base API URL used in all requests
+	 *
+	 * @since 1.1
+	 * @var string
+	 */
+	const BASE_URL = 'http://api.angel.co/1/';
+
+	/**
+	 * AngelList sends a 144x144 blank image for objects without their own image
+	 * Compare against this value if you would like to remove the default image
+	 *
+	 * @since 1.1
+	 * @var string
+	 */
+	const DEFAULT_IMAGE = 'http://angel.co/images/icons/startup-nopic.png';
+
+	/**
+	 * Special HTTP arguments to customize each request to the AngelList API
+	 *
+	 * @since 1.1
+	 * @var array
+	 */
+	public static $http_args = array( 'httpversion' => '1.1', 'redirection' => 0, 'timeout' => 3, 'headers' => array( 'Accept' => 'application/json' ) );
+
+	/**
+	 * AngelList uses a HTTPS URL for static assets such as images to avoid mixed content issues if the parent page is served over HTTPS
+	 * As of May 2012 these images are stored on Amazon S3. If we don't need HTTPS and Amazon's certificate we can construct a new URL based on an assumed CNAME entry for the bucket
+	 * Avoids unncessary overhead of HTTPS when we know we are on HTTP (the majority case) & makes the URL a bit more pretty without the vendor hostname
+	 *
+	 * @since 1.0
+	 * @param string $url AngelList static asset URL
+	 * @return string cleaned up URL if incoming request was HTTP
+	 */
+	public static function filter_static_asset_url( $url ) {
+		if ( is_ssl() ) {
+			// reject including a non-SSL asset on the page if it will generate mixed content warnings
+			return esc_url( $url, array( 'https' ) );
+		} else if ( strlen( $url ) > 41 && substr_compare( $url, 'https://s3.amazonaws.com/photos.angel.co/', 0, 41 ) === 0 )
+			return esc_url( 'http://photos.angel.co/' . substr( $url, 41 ), array( 'http' ) );
+		return esc_url( $url, array( 'http', 'https' ) );
+	}
+
+	/**
+	 * AngelList data for a single company
+	 *
+	 * @since 1.1
+	 * @param int $company_id AngelList company identifer
+	 */
+	public static function get_company( $company_id ) {
+		if ( ! is_int( $company_id ) || $company_id < 1 )
+			return;
+
+		$response = wp_remote_get( AngelList_API::BASE_URL . 'startups/' . $company_id, AngelList_API::$http_args );
+		if ( is_wp_error( $response ) || wp_remote_retrieve_response_code( $response ) != '200' )
+			return;
+
+		$response_body = wp_remote_retrieve_body( $response );
+		if ( empty( $response_body ) )
+			return;
+		return json_decode( $response_body );
+	}
+
+	public static function get_roles_by_company( $company_id ) {
+		if ( ! is_int( $company_id ) || $company_id < 1 )
+			return;
+
+		$response = wp_remote_get( AngelList_API::BASE_URL . 'startup_roles?startup_id=' . $company_id, AngelList_API::$http_args );
+		if ( is_wp_error( $response ) || wp_remote_retrieve_response_code( $response ) != '200' )
+			return;
+
+		$response_body = wp_remote_retrieve_body( $response );
+		if ( empty( $response_body ) )
+			return;
+
+		$json = json_decode( $response_body );
+		if ( ! empty( $json ) && isset( $json->startup_roles ) && ! empty( $json->startup_roles ) )
+			return $json->startup_roles;
+	}
+}
+?>
--- templates/company.php
+++ templates/company.php
@@ -6,7 +6,6 @@
  * @since 1.0
  */
 class AngelList_Company {
-
 	/**
 	 * Identify the unique company, kick off data request
 	 *
@@ -23,10 +22,15 @@ class AngelList_Company {
 		$this->schema_org = (bool) apply_filters( 'angellist_schema_org', true, $this->id );
 
 		// allow override of default browsing context
-		$this->browsing_context = apply_filters( 'angellist_browsing_context', '_blank', $this->id );
+		$browsing_context = apply_filters( 'angellist_browsing_context', '_blank', $this->id );
 		// limit browsing context to special keywords
-		if ( ! in_array( $this->browsing_context, array( '', '_blank', '_self', '_parent', '_top' ), true ) )
-			$this->browsing_context = '_blank';
+		if ( ! in_array( $browsing_context, array( '', '_blank', '_self', '_parent', '_top' ), true ) )
+			$browsing_context = '_blank';
+		if ( $browsing_context )
+			$this->anchor_extra = ' target="' . $browsing_context . '"';
+		else
+			$this->anchor_extra = '';
+		unset( $browsing_context );
 
 		// check for cached version of company info before we request data from AngelList
 		$this->cache_key = $this->generate_cache_key();
@@ -38,44 +42,15 @@ class AngelList_Company {
 	}
 
 	/**
-	 * AngelList uses a HTTPS URL for static assets such as images to avoid mixed content issues if the parent page is served over HTTPS
-	 * As of May 2012 these images are stored on Amazon S3. If we don't need HTTPS and Amazon's certificate we can construct a new URL based on an assumed CNAME entry for the bucket
-	 * Avoids unncessary overhead of HTTPS when we know we are on HTTP (the majority case) & makes the URL a bit more pretty without the vendor hostname
-	 *
-	 * @since 1.0
-	 * @param string $url AngelList static asset URL
-	 * @return string cleaned up URL if incoming request was HTTP
-	 */
-	public static function filter_static_asset_url( $url ) {
-		if ( is_ssl() ) {
-			// reject including a non-SSL asset on the page if it will generate mixed content warnings
-			return esc_url( $url, array( 'https' ) );
-		} else if ( strlen( $url ) > 41 && substr_compare( $url, 'https://s3.amazonaws.com/photos.angel.co/', 0, 41 ) === 0 )
-			return esc_url( 'http://photos.angel.co/' . substr( $url, 41 ), array( 'http' ) );
-		return esc_url( $url, array( 'http', 'https' ) );
-	}
-
-	/**
 	 * Request company data from AngelList.
 	 * Populate data in the class
 	 *
 	 * @since 1.0
 	 */
 	private function populate_data() {
-		$response = wp_remote_get( 'http://api.angel.co/1/startups/' . $this->id, array(
-			'httpversion' => '1.1',
-			'redirection' => 0,
-			'timeout' => 3,
-			'headers' => array( 'Accept' => 'application/json' )
-		) );
-		if ( is_wp_error( $response ) || wp_remote_retrieve_response_code( $response ) != '200' )
-			return;
-
-		$response_body = wp_remote_retrieve_body( $response );
-		if ( empty( $response_body ) )
-			return;
-		$company = json_decode( $response_body );
-		unset( $response_body );
+		if ( ! class_exists( 'AngelList_API' ) )
+			require_once( dirname( dirname( __FILE__ ) ) . '/api.php' );
+		$company = AngelList_API::get_company( $this->id );
 		if ( empty( $company ) )
 			return;
 
@@ -109,10 +84,8 @@ class AngelList_Company {
 			unset( $url );
 		}
 
-		// AngelList sends a 144x144 blank image we don't need
-		$nopic = 'http://angel.co/images/icons/startup-nopic.png';
-		if ( isset( $company->thumb_url ) && $company->thumb_url !== $nopic ) {
-			$url = AngelList_Company::filter_static_asset_url( $company->thumb_url );
+		if ( isset( $company->thumb_url ) && $company->thumb_url !== AngelList_API::DEFAULT_IMAGE ) {
+			$url = AngelList_API::filter_static_asset_url( $company->thumb_url );
 			if ( $url ) {
 				$image = new stdClass();
 				$image->url = $url;
@@ -123,8 +96,8 @@ class AngelList_Company {
 			unset( $url );
 		}
 
-		if ( isset( $company->logo_url ) && $company->thumb_url !== $nopic ) {
-			$url = AngelList_Company::filter_static_asset_url( $company->logo_url );
+		if ( isset( $company->logo_url ) && $company->thumb_url !== AngelList_API::DEFAULT_IMAGE ) {
+			$url = AngelList_API::filter_static_asset_url( $company->logo_url );
 			if ( $url )
 				$this->logo_url = $url;
 			unset( $url );
@@ -161,13 +134,73 @@ class AngelList_Company {
 	}
 
 	/**
+	 * Request a list of people associated with the company from the AngelList API, up to limit (default 3)
+	 *
+	 * @since 1.1
+	 * @param int $limit maximum amount of people
+	 * @return string HTML list of people
+	 */
+	private function people( $limit = 3 ) {
+		// check for garbage
+		if ( ! is_int( $limit ) || $limit < 1 )
+			$limit = 3;
+
+		if ( ! class_exists( 'AngelList_API' ) )
+			require_once( dirname( dirname( __FILE__ ) ) . '/api.php' );
+
+		$people = AngelList_API::get_roles_by_company( $this->id );
+		if ( empty( $people ) || ! is_array( $people ) )
+			return;
+
+		if ( ! class_exists( 'AngelList_Person' ) )
+			require_once( dirname( __FILE__ ) . '/person.php' );
+		$founders = array();
+		$everyone_else = array();
+		foreach ( $people as $person ) {
+			// only care about confirmed, active people
+			if ( ! ( isset( $person->role ) && isset( $person->confirmed ) && $person->confirmed === true && ! isset( $person->ended_at ) ) )
+				continue;
+			if ( $person->role === 'founder' )
+				$founders[] = $person;
+			else
+				$everyone_else[] = $person;
+		}
+		unset( $people );
+
+		$top_people = array();
+		// founders get priority treatment
+		if ( ! empty( $founders ) )
+			$top_people = AngelList_Person::order_by_follower_count( $founders, $limit );
+		$people_count = count( $top_people );
+		if ( $people_count < $limit && ! empty( $everyone_else ) )
+			$top_people = array_merge( $top_people, AngelList_Person::order_by_follower_count( $everyone_else, $limit - $people_count ) );
+
+		// this should not happen. just in case
+		if ( empty( $top_people ) )
+			return;
+
+		$people_html = '';
+		foreach ( $top_people as $person_data ) {
+			$person = new AngelList_Person( $person_data );
+			if ( ! ( isset( $person->name ) && isset( $person->role ) ) )
+				continue;
+			$people_html .= $person->render( $this->schema_org, $this->anchor_extra );
+		}
+		if ( $people_html )
+			return '<ol class="angellist-people">' . $people_html . '</ol>';
+		else
+			return '';
+		
+	}
+
+	/**
 	 * Generate a cache key based on site preferences and SSL requirements
 	 *
 	 * @since 1.0
 	 * @return string WordPress cache or transient key
 	 */
 	private function generate_cache_key() {
-		$cache_parts = array( 'angellist-company', $this->id );
+		$cache_parts = array( 'angellist-company', 'v1.1', $this->id );
 		if ( is_ssl() )
 			$cache_parts[] = 'ssl';
 		if ( isset( $this->schema_org ) && ! $this->schema_org )
@@ -187,12 +220,6 @@ class AngelList_Company {
 		if ( ! ( isset( $this->name ) && isset( $this->profile_url ) ) )
 			return '';
 
-		if ( $this->browsing_context === '' )
-			$anchor_target = ' target="' . $browsing_context . '"';
-		else
-			$anchor_target = '';
-		unset( $browsing_context );
-
 		$profile_url_title_attr = esc_attr( sprintf( __( '%s on AngelList', 'angellist' ), $this->name ) );
 
 		$html = '<li class="angellist-company ';
@@ -201,10 +228,8 @@ class AngelList_Company {
 		else
 			$html .= 'angellist-community-profile';
 		$html .= '" data-startup_id="' . $this->id . '"';
-		if ( $this->schema_org )
-			$html .= ' itemscope itemtype="http://schema.org/Corporation"';
-		$html .= '>';
 		if ( $this->schema_org ) {
+			$html .= ' itemscope itemtype="http://schema.org/Corporation">';
 			if ( isset( $this->url ) )
 				$html .= '<meta itemprop="url" content="' . $this->url . '" />';
 			else
@@ -223,23 +248,25 @@ class AngelList_Company {
 			if ( isset( $this->location_url ) ) {
 				$html .= '<meta itemprop="location" content="' . $this->location_url . '" />';
 			}
+		} else {
+			$html .= '>';
 		}
 
 		$html .= '<div class="angellist-company-summary">';
 		if ( isset( $this->thumbnail ) ) {
-			$html .= '<a class="angellist-company-image" href="' . $this->profile_url . '" title="' . $profile_url_title_attr . '"' . $anchor_target . '>';
+			$html .= '<a class="angellist-company-image" href="' . $this->profile_url . '" title="' . $profile_url_title_attr . '"' . $this->anchor_extra . '>';
 			$html .= '<img alt="' . esc_attr( $this->name ) . '" src="' . $this->thumbnail->url . '" width="90" height="90" />';
 			$html .= '</a>';
 		}
 		$html .= '<div class="angellist-company-summary-text">';
-		$html .= '<a class="angellist-company-name" href="' . $this->profile_url . '" title="' . $profile_url_title_attr . '"' . $anchor_target;
+		$html .= '<a class="angellist-company-name" href="' . $this->profile_url . '" title="' . $profile_url_title_attr . '"' . $this->anchor_extra;
 		if ( $this->schema_org )
 			$html .= ' itemprop="name"';
 		$html .= '>' . esc_html( $this->name ) . '</a>';
 		if ( isset( $this->concept ) )
 			$html .= '<div class="angellist-company-concept">' . esc_html( $this->concept ) . '</div>';
 		$html .= '</div>'; // summary-text
-		$html .= '<span class="angellist-follow-button"><a href="' . $this->profile_url . '" title="' . $profile_url_title_attr . '"' . $anchor_target . '>' . esc_html( __( 'Follow on AngelList', 'angellist' ) ) . '</a></span>';
+		$html .= '<span class="angellist-follow-button"><a href="' . $this->profile_url . '" title="' . $profile_url_title_attr . '"' . $this->anchor_extra . '>' . esc_html( __( 'Follow on AngelList', 'angellist' ) ) . '</a></span>';
 		$html .= '</div>'; // summary
 
 		if ( isset( $this->description ) ) {
@@ -249,6 +276,12 @@ class AngelList_Company {
 			$html .= '<div class="angellist-company-detail"><p>' . implode( '</p><p>', $paragraphs ) . '</p></div>';
 			unset( $paragraphs );
 		}
+
+		// these next features only work for claimed companies
+		if ( $this->claimed ) {
+			$html .= $this->people();
+		}
+
 		$html .= '</li>';
 
 		// cache markup to save us some time on the next request
