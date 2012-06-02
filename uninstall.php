f630591011221cbe75716a6a229c9f02fde775c1
Niall Kennedy <niall@niallkennedy.com>
2012-06-02 02:41:14 +0000

    remove post meta on uninstall

---
+++ uninstall.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Remove all post meta
+ *
+ * @since 1.11
+ */
+function angellist_uninstall() {
+	$meta_key = 'angellist-companies';
+	$all_posts = get_posts(
+		'numberposts' => -1, // everything
+		'post_status' => 'any',
+		'post_type' => 'post',
+		'order_by' => 'none',
+		'meta_query' => $meta_key,
+		'cache_results' => false,
+		'fields' => 'ids'
+	);
+	foreach ( $all_posts as $post_id ) {
+		delete_post_meta( $post_id, $meta_key );
+	}
+}
+
+if ( defined( 'WP_UNINSTALL_PLUGIN' ) )
+	angellist_uninstall();
+?>
