21d7ee82419ee6c154482146d37000fb697e974a
Niall Kennedy <niall@niallkennedy.com>
2013-04-25 12:50:29 -0700

    pass an array of arguments to get_posts. fixes #7

--- uninstall.php
+++ uninstall.php
@@ -7,7 +7,7 @@
  */
 function angellist_uninstall() {
 	$meta_key = 'angellist-companies';
-	$all_posts = get_posts(
+	$all_posts = get_posts( array(
 		'numberposts' => -1, // everything
 		'post_status' => 'any',
 		'post_type' => 'post',
@@ -15,7 +15,7 @@ function angellist_uninstall() {
 		'meta_query' => $meta_key,
 		'cache_results' => false,
 		'fields' => 'ids'
-	);
+	) );
 	foreach ( $all_posts as $post_id ) {
 		delete_post_meta( $post_id, $meta_key );
 	}
